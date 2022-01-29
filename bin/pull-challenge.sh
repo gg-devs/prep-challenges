d372bfa9ad7779eab689331e4049c71558df3910
ahmad-swedani <swedani390@gmail.com>
2022-01-30 00:29:04 +0200

    automaterobot: get the new challenge and test with one command

--- bin/pull-challenge.sh
+++ bin/pull-challenge.sh
@@ -4,9 +4,9 @@ echo "pulling Code Challenge $1"
 
 if [ $1 == '10'  ] || [ $1 == '19'  ]
 then
-    mkdir code-challenges/day-$1
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.nnb > ./code-challenges/day-$1/challenges-$1.nnb
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/README.md > ./code-challenges/day-$1/README.md
+    mkdir code-challenges/challenges-$1
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.nnb > ./code-challenges/challenges-$1/challenges-$1.nnb
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/README.md > ./code-challenges/challenges-$1/README.md
 
 else
     `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
