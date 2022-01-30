3c9d72576768da764f049aa3cba9892f5a5c0206
ahmad-swedani <swedani390@gmail.com>
2022-01-30 11:38:07 +0200

    fix: make pull challenge 15 as a nnb challenge pull

--- bin/pull-challenge.sh
+++ bin/pull-challenge.sh
@@ -2,7 +2,7 @@
 
 echo "pulling Code Challenge $1"
 
-if [ $1 == '10'  ] || [ $1 == '19'  ]
+if [ $1 == '10'  ] || [ $1 == '15'  ] || [ $1 == '19'  ]
 then
     mkdir code-challenges/challenges-$1
     `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.nnb > ./code-challenges/challenges-$1/challenges-$1.nnb
